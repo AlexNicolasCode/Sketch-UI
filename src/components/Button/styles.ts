86680ec076b93a4dccf589107a6c935271239d28
Alex Nicolas <alexoliveira604@gmail.com>
2022-01-09 18:59:28 -0300

    refactor(component): developer must set attributes using only numbers

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 import { Props } from './';
 
 export const Container = styled.button<Props>`
-  width: ${({ w }: Props) => w ?? '150px'};
-  height: ${({ h }: Props) => h ?? '75px'};
-  background-color: ${({ bg }: Props) => bg ?? '#020202'};
-  color: ${({ color }: Props) => color ?? '#f9f9f9'};
+  width: ${({ w }: Props) => w ? `${w}px` : '150px'};
+  height: ${({ h }: Props) => h ? `${h}px` : '75px'};
+  background-color: ${({ bg }: Props) => bg ? `#${bg}` : '#020202'};
+  color: ${({ color }: Props) => color ? `#${color}` : '#f9f9f9'};
   border: ${(props: Props) => getBorder(props)};
   border-radius: ${(props: Props) => getBorderRadius(props)};
 
   @media screen and (min-width: 700px) {
-    width: ${({ wmd }: Props) => wmd ?? '200px'};
-    height: ${({ hmd }: Props) => hmd ?? '100px'};
+    width: ${({ wmd }: Props) => wmd ? `${wmd}px` : '200px'};
+    height: ${({ hmd }: Props) => hmd ? `${hmd}px` : '100px'};
   }
 `;
 
