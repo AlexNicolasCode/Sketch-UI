34ed61fd7809174310949ffd8d6130575db78021
Alex Nicolas <alexoliveira604@gmail.com>
2022-01-09 16:45:59 -0300

    feat: storybook minimal setup

---
+++ .storybook/theme.js
@@ -0,0 +1,15 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: '#50595c',
+
+  colorSecondary: '#50595c',
+
+  appBg: '#0b0a0d',
+  appContentBg: '#16161A',
+  appBorderColor: '#fa3e3e',
+
+  textColor: '#f8f8d6',
+
+  brandTitle: 'Sketch UI',
+});
