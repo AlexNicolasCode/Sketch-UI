a23359b53ec2e121b66ad50bbc3402d2f273bfa1
Alex Nicolas <alexoliveira604@gmail.com>
2022-01-09 20:01:42 -0300

    feat(storybook): Background preset must have dark color on attribute color

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -6,6 +6,6 @@ import { Button } from "../src"
 storiesOf('Button', module)
   .add('Default', () => <Button>Default</Button>)
   .add('Width and Height', () => <Button w='300' h='150' wmd='175' hmd='100'>Width and Height</Button>)
-  .add('Background', () => <Button bg='0202020' color='f9f9f9'>Background</Button>)
+  .add('Background', () => <Button bg='0202020' color='020202'>Background</Button>)
   .add('Border', () => <Button borderSize='20' borderColor='f9f9f9'>Border</Button>)
   .add('rounded', () => <Button rounded roundedSize='20' borderSize='20' borderColor='f9f9f9'>Border</Button>)
