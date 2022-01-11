e08afa8188e5aa58c0b8d3ed812aea5d16aeb6a7
Alex Nicolas <alexoliveira604@gmail.com>
2022-01-10 21:55:52 -0300

    refactor: components must inherit a generic default component

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,63 +1,16 @@
 import React from "react";
-
+import { GenericProps } from "../../types";
+import { GenericComponent } from "../Generic";
 import { Container } from "./styles";
-
-export interface Props {
-    children: any;
-    onClick?: () => void;
-    disabled?: boolean;
-    w?: string;
-    h?: string;
-    wmd?: string;
-    hmd?: string;
-    bg?: string;
-    color?: string;
-    rounded?: boolean;
-    roundedSize?: string,
-    borderSize?: string,
-    borderColor?: string,
-    outline?: boolean,
-    outlineSize?: string,
-    outlineColor?: string,
-}
   
-const Button = ({
-    children,
-    w,
-    h,
-    wmd,
-    hmd,
-    bg,
-    color,
-    rounded,
-    roundedSize,
-    borderSize, 
-    borderColor,
-    outline,
-    outlineSize,
-    outlineColor,
-    ...props
-    }: Props) => {
+const Button = (props: GenericProps) => {
     return (
-        <Container
-            w={w}
-            h={h}
-            hmd={hmd}
-            wmd={wmd}
-            bg={bg}
-            color={color}
-            rounded={rounded}
-            roundedSize={roundedSize}
-            borderSize={borderSize}
-            borderColor={borderColor}
-            outline={outline}
-            outlineSize={outlineSize}
-            outlineColor={outlineColor}
+        <GenericComponent
             {...props}
-        >
-            {children}
-        </Container>
-    );
+            Container={Container}
+            children={"kdaskdsak"}    
+        />
+    )
 };
   
 
