a2d9f92ba6ffb90b6df8fad2cc1189366cfb6dbf
Alex Nicolas <alexoliveira604@gmail.com>
2022-01-10 23:11:24 -0300

    feat: input component can receive attribute to apply styles

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+import { getBackground, getBorder, getBorderRadius, getColor } from '../../generic';
+import { GenericProps } from '../../types';
+
+export const Container = styled.input<GenericProps>`
+  width: ${({ w }: GenericProps) => w ? `${w}px` : '150px'};
+  height: ${({ h }: GenericProps) => h ? `${h}px` : '75px'};
+  background: ${(props: GenericProps) => getBackground(props)};
+  color: ${(props: GenericProps) => getColor(props)};
+  border: ${(props: GenericProps) => getBorder(props)};
+  border-radius: ${(props: GenericProps) => getBorderRadius(props)};
+
+  @media screen and (min-width: 700px) {
+    width: ${({ wmd }: GenericProps) => wmd ? `${wmd}px` : '200px'};
+    height: ${({ hmd }: GenericProps) => hmd ? `${hmd}px` : '100px'};
+  }
+`;
